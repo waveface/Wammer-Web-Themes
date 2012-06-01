4eb5a3b7d1860fbfaab76806f0c9a56e9a59caab
Art <minipai@gmail.com>
2012-06-01 18:17:20 +0800

    fix footer

--- wp/waveface/footer.php
+++ wp/waveface/footer.php
@@ -5,19 +5,33 @@
 
      <div class="terms">
 
-          <a href="about<%=data.vars.extname%>">About Us</a> | 
-          <a href="terms<%=data.vars.extname%>">Terms of Service</a> | 
-          <a href="privacy<%=data.vars.extname%>">Privacy Policy</a>  
-          <p class="copyright">Copyright © 2011 Waveface Inc. All Rights Reserved | 
+    <?php if (get_bloginfo('language') == 'zh-TW'): ?>
 
-          </p>
+          <a href="https://waveface.com/en/about/index.html">About Us</a> | 
+          <a href="https://waveface.com/en/terms.html">Terms of Service</a> | 
+          <a href="https://waveface.com/en/privacy.html">Privacy Policy</a>  
+    <?php else: ?>
+          <a href="https://waveface.com/tw/about/index.html">About Us</a> | 
+          <a href="https://waveface.com/tw/terms.html">Terms of Service</a> | 
+          <a href="https://waveface.com/tw/privacy.html">Privacy Policy</a>  
+
+        
+    <?php endif ?>
+          <p class="copyright">Copyright © 2011 Waveface Inc. All Rights Reserved</p>
 
         </div>
-  	  <div class="socials">
-           <img src="<?php bloginfo('template_directory'); ?>/images/site/icon.fb.png" alt="facebook">
-            <img src="<?php bloginfo('template_directory'); ?>/images/site/icon.twitter.png" alt="twitter">
+      <div class="socials">
+        <a href="https://www.facebook.com/waveface">
+          <img src="<?php bloginfo('template_directory'); ?>/images/site/icon.fb.png" alt="facebook">
+       
+        </a>
+        <a href="https://waveface.com/en/stream/download.html">
+          <img src="<?php bloginfo('template_directory'); ?>/images/site/icon.twitter.png" alt="twitter">         
+        </a>
+        <a href="mailto:contact@waveface.com">
             <img src="<?php bloginfo('template_directory'); ?>/images/site/icon.email.png" alt="email">
-  	  </div>
+        </a>
+      </div>
 
     </div>
   </footer>
